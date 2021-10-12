cea0fa242acefd2ad8647e37af868a969299bb67
Jakub Dzon <jdzon@redhat.com>
2021-10-12 17:22:53 +0200

    Add EdgeDevice controller tests for OBC creation
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,13 +17,17 @@ limitations under the License.
 package controllers
 
 import (
+	"github.com/jakub-dzon/k4e-operator/internal/repository/edgedeployment"
+	"github.com/jakub-dzon/k4e-operator/internal/repository/edgedevice"
+	"github.com/jakub-dzon/k4e-operator/internal/storage"
+	obv1 "github.com/kube-object-storage/lib-bucket-provisioner/pkg/apis/objectbucket.io/v1alpha1"
 	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -37,9 +41,9 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
+var edgeDeviceReconciler *EdgeDeviceReconciler
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -54,7 +58,10 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "config", "crd", "bases"),
+			filepath.Join("..", "config", "test", "crd"),
+		},
 		ErrorIfCRDPathMissing: true,
 	}
 
@@ -65,14 +72,42 @@ var _ = BeforeSuite(func() {
 	err = managementv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = managementv1alpha1.AddToScheme(scheme.Scheme)
+	err = obv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
+
+	edgeDeviceRepository := edgedevice.NewEdgeDeviceRepository(k8sClient)
+	edgeDeviceReconciler = &EdgeDeviceReconciler{
+		Client:               k8sClient,
+		Scheme:               k8sManager.GetScheme(),
+		EdgeDeviceRepository: edgeDeviceRepository,
+		Claimer:              storage.NewClaimer(k8sClient),
+		ObcAutoCreate:        false,
+	}
+	err = edgeDeviceReconciler.SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&EdgeDeploymentReconciler{
+		Client:                   k8sClient,
+		Scheme:                   k8sManager.GetScheme(),
+		EdgeDeviceRepository:     edgeDeviceRepository,
+		EdgeDeploymentRepository: edgedeployment.NewEdgeDeploymentRepository(k8sClient),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
 
 }, 60)
 
