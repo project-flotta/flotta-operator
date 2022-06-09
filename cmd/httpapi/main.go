d119ddaae86b4599aa215c7c925781f2d3a727be
Gloria Ciavarrini <gciavarrini@redhat.com>
2022-06-09 10:15:13 +0200

    Fix after rebase
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- cmd/httpapi/main.go
+++ cmd/httpapi/main.go
@@ -30,9 +30,10 @@ import (
 
 	managementv1alpha1 "github.com/project-flotta/flotta-operator/api/v1alpha1"
 	"github.com/project-flotta/flotta-operator/internal/common/metrics"
+	"github.com/project-flotta/flotta-operator/internal/common/repository/edgedevice"
+	"github.com/project-flotta/flotta-operator/internal/common/repository/playbookexecution"
 	"github.com/project-flotta/flotta-operator/internal/edgeapi"
 	"github.com/project-flotta/flotta-operator/internal/edgeapi/backend/factory"
-	"github.com/project-flotta/flotta-operator/internal/common/repository/playbookexecution"
 	"github.com/project-flotta/flotta-operator/internal/edgeapi/yggdrasil"
 	"github.com/project-flotta/flotta-operator/pkg/mtls"
 	"github.com/project-flotta/flotta-operator/restapi"
@@ -106,6 +107,8 @@ func main() {
 	}
 
 	playbookExecutionRepository := playbookexecution.NewPlaybookExecutionRepository(c)
+	edgeDeviceRepository := edgedevice.NewEdgeDeviceRepository(c)
+
 	metricsObj := metrics.New()
 
 	corev1Client, err := corev1client.NewForConfig(clientConfig)
@@ -130,12 +133,13 @@ func main() {
 	backend, _ := backendFactory.Create(Config)
 
 	yggdrasilAPIHandler := yggdrasil.NewYggdrasilHandler(
-		playbookExecutionRepository,
 		initialDeviceNamespace,
 		metricsObj,
 		mtlsConfig,
 		logger,
 		backend,
+		edgeDeviceRepository,
+		playbookExecutionRepository,
 	)
 
 	var api *operations.FlottaManagementAPI
